9359b6a89cd1864eadc4fbf2ca080b7b81e259b1
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-03-02 03:39:35 +0330

    Initialization

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: {
+    content: ['public/*.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#CE2D4F'
+        },
+        orange: colors.orange,
+        lime: colors.lime,
+        cyan: colors.cyan,
+        blue: colors.lightBlue
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
